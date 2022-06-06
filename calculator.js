cc444ea88c4010723690f5c60f0b800c859ae703
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2022-06-07 03:33:14 +0530

    working web application

--- calculator.js
+++ calculator.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello.. the server is working!");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var sum = num1 + num2;
+  res.send("The result is " + sum);
 });
 
 app.listen(4000, function () {
